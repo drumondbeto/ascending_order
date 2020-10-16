18b57391ba1ff0b16a78fcf926b4849b54d64465
Beto Drumond <humbertowdrumond@gmail.com>
2020-10-16 00:42:27 -0300

    Primeiro commit

---
+++ ascending_order.py
@@ -0,0 +1,22 @@
+# This function checks if the new number already exists in the list
+# If not, it is included.
+def verification(any_number):
+    while any_number in asc_order:
+        any_number = float(input('Please, insert a different number.\n'))
+    asc_order.append(any_number)
+
+
+# A list is created with the first entered number
+asc_order = [float(input('Insert the first number.\n'))]
+
+# The seco
+second_number = float(input('Now, insert the second number.\n'))
+verification(second_number)
+
+third_number = float(input('And, finally, insert the third number.\n'))
+verification(third_number)
+
+asc_order.sort(reverse=False)
+
+print(asc_order)
+
